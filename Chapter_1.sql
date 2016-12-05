2954d605cc961f8df012f84f14b9d12febb638a6
anthonydb <adebarros@gmail.com>
2016-12-04 22:39:00 -0500

    Date update, Chapter 1

--- Chapter_1.sql
+++ Chapter_1.sql
@@ -30,8 +30,8 @@ CREATE TABLE teachers (
 
 INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
 VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
-       ('Lee', 'Reynolds', 'F.D. Roosevelt HS', '1993-5-22', 65000),
-       ('Samuel', 'Cole', 'Myers Middle School', '2005-8-1', 43500),
+       ('Lee', 'Reynolds', 'F.D. Roosevelt HS', '1993-05-22', 65000),
+       ('Samuel', 'Cole', 'Myers Middle School', '2005-08-01', 43500),
        ('Samantha', 'Lee', 'Myers Middle School', '2011-10-30', 36200),
        ('Betty', 'Diaz', 'Myers Middle School', '2005-08-30', 43500),
        ('Kathleen', 'Roush', 'F.D. Roosevelt HS', '2010-10-22', 38500);
