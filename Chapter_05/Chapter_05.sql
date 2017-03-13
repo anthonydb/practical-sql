08e38ee940098fccb0283071cc90e4df7d412edc
anthonydb <adebarros@gmail.com>
2017-03-13 00:11:12 -0400

    Chapter 5 style edits

--- Chapter_05/Chapter_05.sql
+++ Chapter_05/Chapter_05.sql
@@ -16,7 +16,7 @@ SELECT 3 * 4;    -- multiplication
 SELECT 11 / 6;   -- integer division
 SELECT 11 % 6;   -- modulo division
 SELECT 11.0 / 6; -- decimal division
-SELECT CAST(11 AS decimal(3,1)) / 6;
+SELECT CAST(11 AS numeric(3,1)) / 6;
 
 -- Listing 5-3: Exponents, roots and factorials with SQL
 
