875ce1cf6c2d4fc4165670bb364407df50b51d6e
anthonydb <adebarros@gmail.com>
2018-01-08 22:22:08 -0500

    Chapter 14 formatting.

--- Chapter_14/Chapter_14.sql
+++ Chapter_14/Chapter_14.sql
@@ -24,7 +24,7 @@ WHERE srid = 4326;
 
 -- Listing 14-4: Using ST_GeomFromText() to create spatial objects
 
-SELECT ST_GeomFromText('POINT(-74.9 42.7)', 4326);
+SELECT ST_GeomFromText('POINT(-74.9233606 42.699992)', 4326);
 
 SELECT ST_GeomFromText('LINESTRING(-74.9 42.7, -75.1 42.7)', 4326);
 
@@ -34,16 +34,15 @@ SELECT ST_GeomFromText('POLYGON((-74.9 42.7, -75.1 42.7,
 SELECT ST_GeomFromText('MULTIPOINT (-74.9 42.7, -75.1 42.7)', 4326);
 
 SELECT ST_GeomFromText('MULTILINESTRING((-76.27 43.1, -76.06 43.08),
-                                        (-76.2 43.3, -76.2 43.4, -76.4 43.1))',
-                                         4326);
+                                        (-76.2 43.3, -76.2 43.4,
+                                         -76.4 43.1))', 4326);
 
 SELECT ST_GeomFromText('MULTIPOLYGON((
                                      (-74.92 42.7, -75.06 42.71,
                                       -75.07 42.64, -74.92 42.7),
                                      (-75.0 42.66, -75.0 42.64,
                                       -74.98 42.64, -74.98 42.66,
-                                      -75.0 42.66)
-                                    ))', 4326);
+                                      -75.0 42.66)))', 4326);
 
 -- Listing 14-5: Using ST_GeogFromText() to create spatial objects
 
